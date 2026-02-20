36c21e4cef8815aa9cb0ddd022410fae1b3af589
imane.oujja <i.oujja@gmail.com>
2026-02-20 14:39:40 +0100

    Fix case sensitivity

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Introduction from "@/components/introduction";
 import Experience from "@/components/experience";
 import Projects from "@/components/projects";
 import Education from "@/components/education";
-import Programs from "@/components/programs";
+import Programs from "@/components/Programs";
 import Skills from "@/components/skills";
 import Contact from "@/components/contact";
 import Navigation from "@/components/navigation";
